bd8a101977c0ad57667933720e8c38c2c9377d81
technoran <technoran@hotmail.com>
2026-09-18 17:36:24 -0300

    "metodo"

--- script.js
+++ script.js
@@ -70,7 +70,7 @@ const formStep = document.getElementById('bookingFormStep');
 const reviewStep = document.getElementById('bookingReviewStep');
 const paymentStep = document.getElementById('paymentStep');
 const whatsappNumber = '5493854172687';
-const reservationAppUrl = 'https://script.google.com/macros/s/AKfycbx8MXMpWlVPCmDf7dQJq-BPH61cwEbZAEovYuh0rTGcd3COYxaq1zM5k9sal5rLbxPS/exec';
+const reservationAppUrl = 'https://script.google.com/macros/s/AKfycbxozr7sTY-9F5BKppfW7aktNNqyXdCUnh_Lh4jlFLWWZrV5LNNyUHV4GEJA6DvpsCzL/exec';
 
 function setModal(open) {
   modal.classList.toggle('is-open', open);
